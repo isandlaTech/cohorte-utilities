d40eab96ebed90f4c2cf91ce42d542b12fc2fdd3
ogattaz <olivier@gattaz.com>
2014-08-28 15:16:19 +0200

    Addition of constants formatting of the source file

--- src/org/psem2m/utilities/logging/IActivityLoggerBase.java
+++ src/org/psem2m/utilities/logging/IActivityLoggerBase.java
@@ -10,97 +10,109 @@ import org.psem2m.utilities.IXDescriber;
  */
 public interface IActivityLoggerBase extends IXDescriber {
 
-    // Level.ALL => "ALL", Integer.MIN_VALUE
-    public static final String ALL = CActivityLevel.ALL.getName();
-    // Level.CONFIG => "CONFIG", 700,
-    public static final String CONFIG = CActivityLevel.CONFIG.getName();
-    // Same as Level.FINE
-    public static final String DEBUG = CActivityLevel.DEBUG.getName();
-    // Same as Level.SEVERE
-    public static final String ERROR = CActivityLevel.ERROR.getName();
-    // Level.FINE => "FINE", 500
-    public static final String FINE = CActivityLevel.FINE.getName();
-    // Level.FINER => "FINER", 400
-    public static final String FINER = CActivityLevel.FINER.getName();
-    // Level.FINEST => "FINEST", 300
-    public static final String FINEST = CActivityLevel.FINEST.getName();
-    // Level.INFO => "INFO", 800
-    public static final String INFO = CActivityLevel.INFO.getName();
-    // Level.OFF => "OFF", Integer.MAX_VALUE
-    public static final String OFF = CActivityLevel.OFF.getName();
-    // Level.SEVERE => "SEVERE", 1000
-    public static final String SEVERE = CActivityLevel.SEVERE.getName();
-    // Level.WARNING => "WARNING", 900
-    public static final String WARNING = CActivityLevel.WARNING.getName();
+	// Level.ALL => "ALL", Integer.MIN_VALUE
+	public static final String ALL = CActivityLevel.ALL.getName();
 
-    Level getLevel();
+	// Level.CONFIG => "CONFIG", 700,
+	public static final String CONFIG = CActivityLevel.CONFIG.getName();
 
-    /**
-     * @return
-     */
-    public boolean isLogDebugOn();
+	// Same as Level.FINE
+	public static final String DEBUG = CActivityLevel.DEBUG.getName();
 
-    /**
-     * @param aLevel
-     * @return
-     */
-    public boolean isLoggable(Level aLevel);
+	// Same as Level.SEVERE
+	public static final String ERROR = CActivityLevel.ERROR.getName();
 
-    /**
-     * @return
-     */
-    public boolean isLogInfoOn();
+	// Level.FINE => "FINE", 500
+	public static final String FINE = CActivityLevel.FINE.getName();
 
-    /**
-     * @return
-     */
-    public boolean isLogSevereOn();
+	// Level.FINER => "FINER", 400
+	public static final String FINER = CActivityLevel.FINER.getName();
 
-    /**
-     * @return
-     */
-    public boolean isLogWarningOn();
+	// Level.FINEST => "FINEST", 300
+	public static final String FINEST = CActivityLevel.FINEST.getName();
 
-    /**
-     * @param aLevel
-     * @param aWho
-     * @param aWhat
-     * @param aLine
-     */
-    public void log(Level aLevel, Object aWho, CharSequence aWhat,
-            Object... aInfos);
+	// Level.INFO => "INFO", 800
+	public static final String INFO = CActivityLevel.INFO.getName();
 
-    /**
-     * @param record
-     */
-    public void log(LogRecord record);
+	// Level.OFF => "OFF", Integer.MAX_VALUE
+	public static final String OFF = CActivityLevel.OFF.getName();
 
-    /**
-     * @param aWho
-     * @param aWhat
-     * @param aInfos
-     */
-    public void logDebug(Object aWho, CharSequence aWhat, Object... aInfos);
+	public static final String PROP_LOG_LEVEL = "cohorte.isolate.log.level";
 
-    /**
-     * @param aWho
-     * @param aWhat
-     * @param aInfos
-     */
-    public void logInfo(Object aWho, CharSequence aWhat, Object... aInfos);
+	// Level.SEVERE => "SEVERE", 1000
+	public static final String SEVERE = CActivityLevel.SEVERE.getName();
 
-    /**
-     * @param aWho
-     * @param aWhat
-     * @param aInfos
-     */
-    public void logSevere(Object aWho, CharSequence aWhat, Object... aInfos);
+	// Level.WARNING => "WARNING", 900
+	public static final String WARNING = CActivityLevel.WARNING.getName();
 
-    /**
-     * @param aWho
-     * @param aWhat
-     * @param aInfos
-     */
-    public void logWarn(Object aWho, CharSequence aWhat, Object... aInfos);
+	Level getLevel();
+
+	/**
+	 * @return
+	 */
+	public boolean isLogDebugOn();
+
+	/**
+	 * @param aLevel
+	 * @return
+	 */
+	public boolean isLoggable(Level aLevel);
+
+	/**
+	 * @return
+	 */
+	public boolean isLogInfoOn();
+
+	/**
+	 * @return
+	 */
+	public boolean isLogSevereOn();
+
+	/**
+	 * @return
+	 */
+	public boolean isLogWarningOn();
+
+	/**
+	 * @param aLevel
+	 * @param aWho
+	 * @param aWhat
+	 * @param aLine
+	 */
+	public void log(Level aLevel, Object aWho, CharSequence aWhat,
+			Object... aInfos);
+
+	/**
+	 * @param record
+	 */
+	public void log(LogRecord record);
+
+	/**
+	 * @param aWho
+	 * @param aWhat
+	 * @param aInfos
+	 */
+	public void logDebug(Object aWho, CharSequence aWhat, Object... aInfos);
+
+	/**
+	 * @param aWho
+	 * @param aWhat
+	 * @param aInfos
+	 */
+	public void logInfo(Object aWho, CharSequence aWhat, Object... aInfos);
+
+	/**
+	 * @param aWho
+	 * @param aWhat
+	 * @param aInfos
+	 */
+	public void logSevere(Object aWho, CharSequence aWhat, Object... aInfos);
+
+	/**
+	 * @param aWho
+	 * @param aWhat
+	 * @param aInfos
+	 */
+	public void logWarn(Object aWho, CharSequence aWhat, Object... aInfos);
 
 }
