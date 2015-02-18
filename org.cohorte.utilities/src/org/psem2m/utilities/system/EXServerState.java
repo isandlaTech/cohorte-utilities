ad7b51447a707fc78651e13087cbff9713e04a86
ogattaz <olivier@gattaz.com>
2015-02-18 17:43:28 +0100

    Add utils methods

--- org.cohorte.utilities/src/org/psem2m/utilities/system/EXServerState.java
+++ org.cohorte.utilities/src/org/psem2m/utilities/system/EXServerState.java
@@ -6,4 +6,45 @@ package org.psem2m.utilities.system;
  */
 public enum EXServerState {
 	EXCEPTION, INSTANCIATED, STARTED, STARTING, STOPPED, STOPPING, TIMEOUT;
+
+	/**
+	 * @param aText
+	 * @return
+	 */
+	public static EXServerState stdoutToState(final String aText) {
+
+		if (aText != null && !aText.isEmpty()) {
+
+			String wUpperText = aText.toUpperCase();
+
+			for (EXServerState wState : EXServerState.values()) {
+				if (wUpperText.startsWith(wState.name())) {
+					return wState;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @return
+	 */
+	boolean isOnError() {
+		return this == EXCEPTION;
+	}
+
+	/**
+	 * @return
+	 */
+	boolean isStarted() {
+		return this == STARTED;
+	}
+
+	/**
+	 * @return
+	 */
+	boolean isStopped() {
+		return this == STOPPED;
+	}
+
 }
