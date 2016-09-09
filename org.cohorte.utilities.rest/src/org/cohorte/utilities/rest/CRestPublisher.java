bc04e737fa33e8d3493707d90b703dd5ad824e3b
ogattaz <olivier@gattaz.com>
2016-09-09 19:24:02 +0200

    MOD_OG_20160906 Enhancement of the trace. Modification of the requirement of the LogService to select the cohorte Logservice

--- org.cohorte.utilities.rest/src/org/cohorte/utilities/rest/CRestPublisher.java
+++ org.cohorte.utilities.rest/src/org/cohorte/utilities/rest/CRestPublisher.java
@@ -1,5 +1,7 @@
 package org.cohorte.utilities.rest;
 
+import static org.osgi.framework.Constants.SERVICE_DESCRIPTION;
+
 import java.util.Dictionary;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -16,6 +18,7 @@ import org.apache.felix.ipojo.annotations.Validate;
 import org.cohorte.utilities.rest.api.IRestApplication;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
@@ -25,50 +28,56 @@ import org.osgi.service.log.LogService;
  * REST API provider. This component aggregates all registered REST API modules
  * and register them into an OSGi HTTP Service whenever such a service is made
  * available.
- * 
+ *
  * @author Ahmad Shahwan
  *
  */
-@Component(name="Cohorte-Rest")
+@Component(name = "Cohorte-Rest")
 public class CRestPublisher {
-	
-	/**
-	 * A set of REST modules awaiting to be registered, once the component is
-	 * validated.
-	 */
-	private Set<IRestApplication> pAwaiting = new HashSet<>();
-	
-	/**
-	 * Whether the component is valid.
-	 */
-	private boolean pValid = false;
-	
+
 	/**
 	 * OSGi HTTP Service.
 	 */
 	@Requires
 	private HttpService httpService = null;
-	
+
+	/**
+	 * A set of REST modules awaiting to be registered, once the component is
+	 * validated.
+	 */
+	private Set<IRestApplication> pAwaiting = new HashSet<>();
+
+	private final BundleContext pBundleContext;
+
 	/**
 	 * OSGi Logger.
 	 */
-	@Requires(optional=true)
-	LogService pLogger = null;
-	
+	@Requires(proxy = false, filter = "(" + SERVICE_DESCRIPTION + "=cohorte)")
+	private LogService pLogger = null;
+
+	/**
+	 * Whether the component is valid.
+	 */
+	private boolean pValid = false;
+
 	/**
 	 * No-args constructor.
 	 */
-	public CRestPublisher() {
+	public CRestPublisher(final BundleContext aBundleContext) {
+		super();
+		pBundleContext = aBundleContext;
+
 	}
-	
+
 	/**
 	 * Callback that binds a REST module.
-	 * 
-	 * @param aApplication REST module
+	 *
+	 * @param aApplication
+	 *            REST module
 	 */
-	@Bind(aggregate=true)
+	@Bind(aggregate = true)
 	private void bind(IRestApplication aApplication) {
-		this.pLogger.log(LogService.LOG_INFO, "Binding to a REST application.");
+		pLogger.log(LogService.LOG_INFO, "Binding to a REST application.");
 		boolean valid = false;
 		synchronized (this) {
 			if (this.pValid) {
@@ -78,108 +87,129 @@ public class CRestPublisher {
 				this.pAwaiting.add(aApplication);
 			}
 		}
-		this.pLogger.log(LogService.LOG_INFO,
-				valid ? "Valid application." : "Invalid application.");
+		pLogger.log(LogService.LOG_INFO, valid ? "Valid application."
+				: "Invalid application.");
 	}
-	
+
+	/**
+	 * Initialize Jersey servlet init parameters.
+	 *
+	 * @param module
+	 * @return
+	 */
+	private Dictionary<String, String> getInitParams(IRestApplication module) {
+		Dictionary<String, String> jerseyServletParams = new Hashtable<>();
+		jerseyServletParams.put(ServletProperties.JAXRS_APPLICATION_CLASS,
+				module.getClass().getName());
+		return jerseyServletParams;
+	}
+
+	/**
+	 * Invalidate callback.
+	 */
+	@Invalidate
+	private void invalidate() {
+		pLogger.log(LogService.LOG_DEBUG, "Invalidating REST provider");
+		this.pValid = false;
+	}
+
+	/**
+	 * Register a REST module.
+	 *
+	 * @param aApplication
+	 */
+	private boolean register(IRestApplication aApplication) {
+
+		String wModulePath = aApplication.getPath();
+		ClassLoader classLoader = aApplication.getClass().getClassLoader();
+		ClassLoader originalContextClassLoader = Thread.currentThread()
+				.getContextClassLoader();
+		try {
+			// Work around issue JERSEY-2093
+			Thread.currentThread().setContextClassLoader(classLoader);
+
+			httpService.registerServlet(wModulePath, new ServletContainer(),
+					getInitParams(aApplication), null);
+
+			pLogger.log(LogService.LOG_INFO,
+					String.format("REST module [%s] registered.", wModulePath));
+
+			return true;
+
+		} catch (IllegalStateException e) {
+			pLogger.log(
+					LogService.LOG_ERROR,
+					String.format(
+							"REST Registration State error while registering module [%s]",
+							wModulePath), e);
+		} catch (ServletException | NamespaceException e) {
+			pLogger.log(
+					LogService.LOG_ERROR,
+					String.format(
+							"ServletException of NamespaceException occurred while registering module [%s]",
+							wModulePath), e);
+		} catch (Exception | Error e) {
+			pLogger.log(LogService.LOG_ERROR, String.format(
+					"REST Registration error while registering module [%s]",
+					wModulePath), e);
+		} finally {
+
+			// restore
+			Thread.currentThread().setContextClassLoader(
+					originalContextClassLoader);
+		}
+		// if pb
+		return false;
+	}
+
 	/**
 	 * Validate callback.
 	 */
 	@Validate
 	private void validate() {
 		try {
-			this.pLogger.log(LogService.LOG_INFO, "Validating REST provider.");
-			this.pLogger.log(LogService.LOG_INFO, 
-					String.format("Number of awaiting REST modules: %d .",
-							pAwaiting.size()));
-			this.pLogger.log(LogService.LOG_INFO,
-					"(Re-)starting resource locator bundle.");
+			System.out.println(String.format("pLogger=[%s]", pLogger));
+
+			pLogger.log(LogService.LOG_INFO, "Validating REST provider.");
+
 			/*
 			 * The bundle {@code org.glassfish.hk2.osgiresourcelocator} must be
-			 * started before Servlet registration.
-			 * Fix for {@code java.lang.IllegalStateException} with message: 
-			 * "No generator was provided and there is no default generator
-			 * registered"
+			 * started before Servlet registration. Fix for {@code
+			 * java.lang.IllegalStateException} with message: "No generator was
+			 * provided and there is no default generator registered"
 			 * 
 			 * @see https://java.net/jira/browse/JERSEY-2788
 			 */
+			pLogger.log(LogService.LOG_INFO,
+					"(Re-)starting resource locator bundle.");
 			FrameworkUtil.getBundle(
-				org.glassfish.hk2.osgiresourcelocator.Activator.class
-			).start();
+					org.glassfish.hk2.osgiresourcelocator.Activator.class)
+					.start();
+
+			pLogger.log(LogService.LOG_INFO,
+					String.format("Number of awaiting REST modules: [%d] .",
+							pAwaiting.size()));
+
 			synchronized (this) {
-				for (Iterator<IRestApplication> i = this.pAwaiting.iterator();
-						i.hasNext();) {
+				for (Iterator<IRestApplication> i = this.pAwaiting.iterator(); i
+						.hasNext();) {
 					if (register(i.next())) {
 						i.remove();
 					}
 				}
 			}
-			if (this.pAwaiting.size() == 0) {
-				this.pValid = true;
+
+			int wNbRemain = pAwaiting.size();
+			if (wNbRemain == 0) {
+				pValid = true;
 			} else {
-				this.pLogger.log(LogService.LOG_WARNING,
-						"Not all REST application have been registered");
+				pLogger.log(LogService.LOG_WARNING,
+						"Not all REST application have been registered. remain="
+								+ wNbRemain);
 			}
 		} catch (Throwable e) {
-			this.pLogger.log(
-					LogService.LOG_ERROR, "REST Registration error.", e);
+			pLogger.log(LogService.LOG_ERROR, "REST Registration error.", e);
 			e.printStackTrace();
 		}
-	}
-	
-	/**
-	 * Invalidate callback.
-	 */
-	@Invalidate
-	private void invalidate() {
-		this.pLogger.log(LogService.LOG_DEBUG, "Invalidating REST provider");
-		this.pValid = false;
-	}
-	
-	/**
-	 * Register a REST module.
-	 * 
-	 * @param aApplication
-	 */
-	private boolean register(IRestApplication aApplication) {
-		boolean result = true;
-		ClassLoader classLoader = aApplication.getClass().getClassLoader();
-        ClassLoader originalContextClassLoader =
-        		Thread.currentThread().getContextClassLoader();
-		try {
-			// Work around issue JERSEY-2093
-	        Thread.currentThread().setContextClassLoader(classLoader);
-			httpService.registerServlet(
-					aApplication.getPath(),
-					new ServletContainer(),
-					getInitParams(aApplication), null);
-			this.pLogger.log(LogService.LOG_INFO, "REST module registered.");
-		} catch (IllegalStateException e) { 
-			this.pLogger.log(
-					LogService.LOG_ERROR, "REST Registration error.", e);
-			result = false;
-		} catch (ServletException | NamespaceException e) {
-			this.pLogger.log(LogService.LOG_ERROR,
-					"Exception occurred while registering REST module.", e);
-		}
-		finally {
-			Thread.currentThread()
-				.setContextClassLoader(originalContextClassLoader);
-		}
-		return result;
-	}
-
-	/**
-	 * Initialize Jersey servlet init parameters.
-	 * 
-	 * @param module
-	 * @return
-	 */
-	private Dictionary<String, String> getInitParams(IRestApplication module) {
-		Dictionary<String, String> jerseyServletParams = new Hashtable<>();
-		jerseyServletParams.put(
-				ServletProperties.JAXRS_APPLICATION_CLASS,
-				module.getClass().getName());
-		return jerseyServletParams;
 	}
 }
