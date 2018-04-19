ab05305a63aeec7129df9e95a9445315a1e0378c
apisu <apisu@MBPdeAurelien2.office>
2018-04-19 17:13:11 +0200

    Revert " change http to https"
    This reverts commit 3f47bf721e1fde86c4cb8c934da8f204d8ff988e.

--- extra/org.cohorte.utilities.extra.p2/upload.sh
+++ extra/org.cohorte.utilities.extra.p2/upload.sh
@@ -14,7 +14,7 @@ cd $DIR/target/repository
 
 for file in ./*
 do
-	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
+	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} http://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
 done;
 
 cd $DIR/../../Build/maven/p2-repo
@@ -24,10 +24,10 @@ echo "-----------"
 
 for file in ./*
 do
-	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
+	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} http://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
 done;
 
 for file in plugins/*
 do
-	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} https://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
+	curl -v --user "$USERNAME:$PASSWORD" --upload-file ${file} http://nrm.cohorte.tech/repository/${REPO_NAME}/${file}
 done;
