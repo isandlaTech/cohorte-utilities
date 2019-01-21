24042438d636909f871e2f99e652758ce55f8565
pisua <aurelien.pisu@cohorte-technologies.com>
2019-01-21 10:08:08 +0100

    add new lib for transform json to CSV

---
+++ ThirdPartiesLibs/org.cohorte.utilities.libs.com.github.json2flat/src/org/cohorte/utilities/libs/com/github/json2flat/Activator.java
@@ -0,0 +1,46 @@
+package org.cohorte.utilities.libs.com.github.json2flat;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(final BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		Bundle wBundle = context.getBundle();
+
+		System.out.printf("%50s | Bundle=[%50s][%s] started\n",
+				"Activator.start()", wBundle.getSymbolicName(),
+				wBundle.getVersion());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(final BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		Bundle wBundle = context.getBundle();
+		System.out.printf("%50s | Bundle=[%50s][%s] stopped\n",
+				"Activator.stop()", wBundle.getSymbolicName(),
+				wBundle.getVersion());
+	}
+}
