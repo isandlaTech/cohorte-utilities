4c53e2f7b523114f1125fe14eb78b3beddae08fe
ogattaz <olivier.gattaz@cohorte-technologies.com>
2019-03-19 00:11:56 +0100

    #31 Major enhancement of the Json tooling    - Convert JSONException to be a RuntimeException    - JSONObject and JSONArray implement clonable    - new JSONBuilder

--- org.cohorte.utilities/src/org/psem2m/utilities/json/JSONException.java
+++ org.cohorte.utilities/src/org/psem2m/utilities/json/JSONException.java
@@ -2,29 +2,68 @@ package org.psem2m.utilities.json;
 
 /**
  * The JSONException is thrown by the JSON.org classes then things are amiss.
+ * 
+ * OG - #31
+ * 
  * @author JSON.org
- * @version 2
+ * @version 3
  */
-public class JSONException extends Exception {
-    // --------------------------------------------------------------------
-    private static final long serialVersionUID=1L;
-    private Throwable cause;
+public class JSONException extends RuntimeException {
 
-    /**
-     * Constructs a JSONException with an explanatory message.
-     * @param message Detail about the reason for the exception.
-     */
-    public JSONException(String message) {
-        super(message);
-    }
+	private static final long serialVersionUID = -5669685299402172321L;
 
-    public JSONException(Throwable t) {
-        super(t.getMessage());
-        this.cause = t;
-    }
+	/**
+	 * @param aCause
+	 * @return
+	 */
+	private static String buildCauseMessage(final Throwable aCause) {
+		return String.format("json exception due to %s", aCause.getClass().getSimpleName());
+	}
 
-    @Override
-	public Throwable getCause() {
-        return this.cause;
-    }
+	/**
+	 * Constructs a JSONException with an explanatory message.
+	 * 
+	 * @param message
+	 *            The message to embed in the json exception.
+	 */
+	public JSONException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a JSONException with an explanatory message.
+	 * 
+	 * @param aFormat
+	 *            the format of the message to embed in the json exception.
+	 * @param aArgs
+	 *            The arguments to include in the format of the message
+	 */
+	public JSONException(String aFormat, final Object... aArgs) {
+		super(String.format(aFormat, aArgs));
+	}
+
+	/**
+	 * Constructs a JSONException with a cause.
+	 * 
+	 * @param aCause
+	 *            The cause of the json exception.
+	 */
+	public JSONException(final Throwable aCause) {
+		super(buildCauseMessage(aCause), aCause);
+	}
+
+	/**
+	 * Constructs a JSONException with a cause and an explanatory message.
+	 * 
+	 * @param aCause
+	 *            The cause of the json exception.
+	 * @param aFormat
+	 *            the format of the message to embed in the json exception.
+	 * @param aArgs
+	 *            The arguments to include in the format of the message.
+	 */
+	public JSONException(final Throwable aCause, final String aFormat, final Object... aArgs) {
+		super(String.format(aFormat, aArgs), aCause);
+	}
+
 }
