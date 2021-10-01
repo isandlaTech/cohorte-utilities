ce46e152154eed55b46e3ae8382ff75405ef365e
apisu <aurelien.pisu@cohorte-technologies.com>
2021-10-01 14:46:29 +0200

    add -k to curl to bypass verification

--- extra/org.cohorte.utilities.extra.p2/create.sh
+++ extra/org.cohorte.utilities.extra.p2/create.sh
@@ -14,12 +14,12 @@ echo "pass=$pass"
 echo "repo=$repo"
 printf "Deleting previous $name script...\n\n"
 
-curl -v -X DELETE -u $user:$pass "https://nrm.cohorte.tech/service/siesta/rest/v1/script/$name"
+curl -k -v -X DELETE -u $user:$pass "https://nrm.cohorte.tech/service/siesta/rest/v1/script/$name"
 
 printf "Uploading $name script. It contains: $jsonFile\n\n"
 
-curl -v -u $user:$pass --header "Content-Type: application/json" 'https://nrm.cohorte.tech/service/siesta/rest/v1/script/' -d @$jsonFile
+curl -k -v -u $user:$pass --header "Content-Type: application/json" 'https://nrm.cohorte.tech/service/siesta/rest/v1/script/' -d @$jsonFile
 
 printf "Running $name script..."
 
-curl -v -X POST -u $user:$pass --header "Content-Type: text/plain" "https://nrm.cohorte.tech/service/siesta/rest/v1/script/$name/run" -d $repo
+curl -k -v -X POST -u $user:$pass --header "Content-Type: text/plain" "https://nrm.cohorte.tech/service/siesta/rest/v1/script/$name/run" -d $repo
