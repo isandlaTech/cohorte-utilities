b2d1f37b97e2dd12fb8b544a984287e84ee34cab
apisu <aurelien.pisu@cohorte-technologies.com>
2022-03-25 17:09:19 +0100

    add CSchemaValidator

---
+++ ThirdPartiesLibs/org.cohorte.libs.com.networknt.schema/src/org/cohorte/iot/json/validator/api/CSchemasValidator.java
@@ -0,0 +1,36 @@
+package org.cohorte.iot.json.validator.api;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.psem2m.utilities.json.JSONObject;
+import org.psem2m.utilities.logging.IActivityLogger;
+
+public class CSchemasValidator {
+
+	private final ConcurrentHashMap<String, CJsonSchema> pMapIdSchema = new ConcurrentHashMap<String,CJsonSchema>();
+	private final IActivityLogger pLogger;
+	public CSchemasValidator(final IActivityLogger aLogger){
+		pLogger = aLogger;
+	}
+
+	public void addSchema(final String aId, final JSONObject aSchema) throws Exception {
+		final CJsonSchema wSchema = CJsonValidatorFactory.getSingleton().getSchema(pLogger, aSchema);
+		pMapIdSchema.put(aId, wSchema);
+	}
+
+	public CJsonSchema deleteSchema(final String aId) throws Exception {
+		return pMapIdSchema.remove(aId);
+	}
+	public CJsonSchema getSchema(final String aId) {
+		return pMapIdSchema.get(aId);
+	}
+	public boolean hasSchema(final String aId) {
+		return pMapIdSchema.containsKey(aId);
+	}
+	public boolean validateJson(final String aIdSchema, final JSONObject aDataToValidate) throws Exception{
+		if( !hasSchema(aIdSchema) ) {
+			throw new Exception(String.format("can't find schema for id %s",aIdSchema));
+		}
+		return CJsonValidatorFactory.getSingleton().valdate(pLogger, getSchema(aIdSchema), aDataToValidate);
+	}
+}
