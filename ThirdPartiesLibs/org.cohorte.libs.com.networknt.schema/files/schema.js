1913381ae4f0a2b4d17aaa39de40251754f68944
apisu <aurelien.pisu@cohorte-technologies.com>
2022-03-28 12:25:58 +0200

    add schema test

--- ThirdPartiesLibs/org.cohorte.libs.com.networknt.schema/files/schema.js
+++ ThirdPartiesLibs/org.cohorte.libs.com.networknt.schema/files/schema.js
@@ -1,51 +1,85 @@
-{"type":"object",
-	"$schema": "http://json-schema.org/draft-03/schema",
-	"id": "#",
-	"required":false,
-	"properties":{
-		"OrgId": {
-			"type":"string",
-			"id": "OrgId",
-			"required":false
-		},
-		"metrics": {
-			"type":"array",
-			"id": "metrics",
-			"required":false,
-			"items": {
-				"type":"object",
-				"id": "0",
-				"required":false,
-				"properties":{
-					"name": {
-						"type":"string",
-						"id": "name",
-						"required":false
-					},
-					"valueTrend": {
-						"type":"array",
-						"id": "valueTrend",
-						"required":false,
-						"items": {
-							"type":"object",
-							"id": "0",
-							"required":false,
-							"properties":{
-								"date": {
-									"type":"string",
-									"id": "date",
-									"required":false
-								},
-								"val": {
-									"type":"string",
-									"id": "val",
-									"required":false
-								}
-							}
-						}
-					}
+{
+	"properties": {
+		"_iot": {
+			"id": "/properties/_iot",
+			"properties": {
+				"tid": {
+					"type": "string"
+				},
+				"mat": {
+					"type": "number"
+				},
+				"cat": {
+					"type": "number"
+				},
+				"mby": {
+					"type": "string"
+				},
+				"cby": {
+					"type": "string"
+				},
+				"item_id": {
+					"type": "string"
+				},
+				"hash": {
+					"type": "string"
 				}
-			}
+			},
+			"type": "object"
+		},
+		"transcripterId": {
+			"type": "string"
+		},
+		"notSynchronizable": {
+			"type": "boolean"
+		},
+		"cmt": {
+			"id": "/properties/cmt",
+			"type": "string"
+		},
+		"_id": {
+			"id": "/properties/id",
+			"type": "string",
+			"pattern": "^(\\w|-|_|.)*§"
+		},
+		"kwd": {
+			"id": "/properties/kwd",
+			"items": {
+				"id": "/properties/kwd/items",
+				"type": "string"
+			},
+			"type": "array"
+		},
+		"lbl": {
+			"id": "/properties/lbl",
+			"type": "string"
+		},
+		"md": {
+			"id": "/properties/md",
+			"anyOf": [{
+				"type": "integer"
+			}, {
+				"type": "number"
+			}, {
+				"type": "string"
+			}, {
+				"type": "array"
+			}, {
+				"type": "boolean"
+			}, {
+				"type": "object"
+			}, {
+				"type": "null"
+			}],
+			"type": "object"
+		},
+		"scope": {
+			"id": "/properties/scope",
+			"type": "string"
+		},
+		"permission": {
+			"id": "/properties/permission",
+			"type": "string"
 		}
 	}
 }
